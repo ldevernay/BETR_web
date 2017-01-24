c5af5a38b493159f64df39000f42d6d3b147f573
ldevernay <boblehamster@yahoo.fr>
2017-01-24 12:07:25 +0100

    add : typeform in about.html

--- form-handler-nodb.php
+++ form-handler-nodb.php
@@ -2,19 +2,21 @@
 // Emails form data to you and the person submitting the form
 // This version requires no database.
 // Set your email below
-$myemail = "ENTER_YOUR_EMAIL_HERE"; // Replace with your email, please
+// FIXME
+$myemail = "ldevernay@simplon.co"; // Replace with your email, please
 
 // Receive and sanitize input
 $name = $_POST['name'];
+$society = $_POST['society'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // set up email
-$msg = "New contact form submission!\nName: " . $name . "\nEmail: " . $email . "\nPhone: " . $phone . "\nEmail: " . $email;
+$msg = "Nouveau contact via le site!\nNom: " . $name . "\nSociété: " . $society . "\nEmail: " . $email . "\nNuméro de téléphone: " . $phone . "\nEmail: " . $email;
 $msg = wordwrap($msg,70);
-mail($myemail,"New Form Submission",$msg);
-mail($email,"Thank you for your form submission",$msg);
-echo 'Thank you for your submission.  Please <a href="index.html">Click here to return to our homepage.';
+mail($myemail,"Nouveau contact via le site",$msg);
+mail($email,"Merci pour votre message. Nous vous contacterons dès que possible.",$msg);
+echo 'Merci pour votre message. <a href="index.html">Cliquez ici pour retourner sur le site.</a>";
 
 ?>
